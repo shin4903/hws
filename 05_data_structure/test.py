493f57663ad987762fecb98d38207245029779ac
신종혁 <shinjh4903@gmail.com>
2023-01-25 17:01:50 +0900

    수업끝

---
+++ 05_data_structure/test.py
@@ -0,0 +1,23 @@
+# dust = [10, 7, 2, 19]
+
+# # print(dust.sort())
+# # print(dust)
+# print(sorted(dust))
+
+# string = 'Vi-tor'
+# print(string.replace('-', 'k'))
+# print(string)
+
+# dust = []
+# dust.append('00')
+# print(dust)
+# dust.extend('00')
+# print(dust)
+
+# dust = {'key': 'value'}
+# print(dust.get('samsung', 'galaxy'))
+# print(dust['apple'])
+
+dust = [1, 2, 10]
+print(dust.pop())
+print(dust)
