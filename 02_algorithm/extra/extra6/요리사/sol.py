c0c05ca93c66d74d3ce91b04f1834ecd97033d68
신종혁 <shinjh4903@gmail.com>
2023-04-03 17:49:42 +0900

    230403 class end

--- 02_algorithm/extra/extra6/요리사/sol.py
+++ 02_algorithm/extra/extra6/요리사/sol.py
@@ -1,13 +1,16 @@
 import sys
 sys.stdin = open('input.txt')
 
-def back(i,lst):
-    if i == N:
-        if len(lst) == 4:
-            result.append(lst)
+def back(n, i, tmp):
+    if n == N -1 :
+        result.append(tmp)
         return
-    back(i + 1, lst + [A[i]])
-    back(i+1, lst)
+
+    for j in range(N):
+        if i != j and not visited[j]:
+            visited[j] = 1
+            back(n+1, j, tmp + arr[i][j] + arr[j][i])
+            visited[j] = 0
 
 T = int(input())
 
@@ -16,5 +19,6 @@ for tc in range(1,T+1):
     arr = [list(map(int,input().split())) for _ in range(N)]
     A = [i for i in range(N)]
     result = []
-    back(0,[])
+    visited = [0] * N
+    back(0,0,0)
     print(result)
--- 03_algorithm_II/04_Exhaustive_Search/230327/5189_전자카트/sol.py
+++ 03_algorithm_II/04_Exhaustive_Search/230327/5189_전자카트/sol.py
@@ -1,8 +1,29 @@
 import sys
 sys.stdin = open('input.txt')
 
+def back(n, i, tmp):
+    global result
+    if n == N-1:
+        tmp += arr[i][0]
+        result = min(result, tmp)
+        return
+
+    if tmp >= result:
+        return
+
+    for j in range(1,N):
+        if i != j and not visited[j]:
+            visited[j] = 1
+            back(n+1, j, tmp + arr[i][j])
+            visited[j] = 0
+
 T = int(input())
 
 for tc in range(1,T+1):
     N = int(input())
-    arr = [list(map(int,input().split()))]
+    arr = [list(map(int,input().split())) for _ in range(N)]
+    result = 100*N*N
+    visited = [0] * N
+
+    back(0,0,0)
+    print(f'#{tc}',result)
