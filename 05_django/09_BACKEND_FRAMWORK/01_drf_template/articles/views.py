ba43a502f1511376c08eab854b22e08ddfa658ec
신종혁 <shinjh4903@gmail.com>
2023-04-17 17:44:42 +0900

    230417 cless end

---
+++ 05_django/09_BACKEND_FRAMWORK/01_drf_template/articles/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import get_list_or_404, get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Article, Comment
+from .serializers import ArticleListSerializer, CommentSerializer
+
+# Create your views here.
+@api_view(['GET', 'POST'])
+def article_list(request):
+    if request.method == 'GET':
+        articles = get_list_or_404(Article)
+        serializer = ArticleListSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ArticleListSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET','DELETE', 'PUT'])
+def article_detail(request, article_pk):
+    article = get_list_or_404(Article, pk = article_pk)
+    if request.method =='GET':  
+        serializer = ArticleListSerializer(article)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    elif request.method =='PUT':
+        serializer = ArticleListSerializer(article, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        
+# @api_view(['GET'])
+# def comments_list(requset):
+#     if requset.method == 'GET':
+#         comments = Comment.objects.all()
+#         serializer = CommentSerializer(comments, many=True)
+#         return Response(serializer.data)
+    
+@api_view(['GET','POST']) 
+def comment_list(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+
+    if request.method == 'GET':
+        comments = article.comment_set.all()
+        serializer = CommentSerializer(comments,many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(article=article)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+
+        
+@api_view(['GET'])
+def comment_detail(request, comment_pk):
+    if request.method == 'GET':
+        comment = Comment.objects.get(pk = comment_pk)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
