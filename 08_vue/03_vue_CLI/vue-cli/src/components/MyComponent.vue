47e4c7b8b8c8135420e92b2012a1fa24b02f93a6
신종혁 <shinjh4903@gmail.com>
2023-05-04 17:51:31 +0900

    230504 class end

---
+++ 08_vue/03_vue_CLI/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="border">
+    <h1>This is Component</h1>
+    <MyChild />
+    <MyChild />
+    <MyChild />
+    <MyChild />
+  </div>
+  
+</template>
+
+<script>
+import MyChild from '@/components/MyChild'
+
+
+export default {
+    name : 'MyComponent',
+    components : {
+        MyChild,
+    }
+}
+</script>
+
+<style>
+    .border{
+        border: solid;
+    }
+</style>
